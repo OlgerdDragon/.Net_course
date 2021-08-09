9d3f2f260439d83129224a5ccaa6854577b4a82e
Vlad Bilokon <olgerddragon@gmail.com>
2021-08-09 12:20:50 +0300

    add

--- ProducerVSConsumer/Program.cs
+++ ProducerVSConsumer/Program.cs
@@ -32,11 +32,9 @@ namespace ProducerVSConsumer
 
         static void ConsumerJob()
         {
-            // Make sure we get a different random seed from the
-            // first thread
+            
             Random rng = new Random(1);
-            // We happen to know we've only got 10 
-            // items to receive
+            
             for (int i = 0; i < 10; i++)
             {
                 object o = queue.Consume();
@@ -57,11 +55,7 @@ namespace ProducerVSConsumer
             {
                 queue.Enqueue(o);
 
-                // We always need to pulse, even if the queue wasn't
-                // empty before. Otherwise, if we add several items
-                // in quick succession, we may only pulse once, waking
-                // a single thread up, even if there are multiple threads
-                // waiting for items.            
+                      
                 Monitor.Pulse(listLock);
             }
         }
@@ -70,15 +64,10 @@ namespace ProducerVSConsumer
         {
             lock (listLock)
             {
-                // If the queue is empty, wait for an item to be added
-                // Note that this is a while loop, as we may be pulsed
-                // but not wake up before another thread has come in and
-                // consumed the newly added object. In that case, we'll
-                // have to wait for another pulse.
+                
                 while (queue.Count == 0)
                 {
-                    // This releases listLock, only reacquiring it
-                    // after being woken up by a call to Pulse
+                    
                     Monitor.Wait(listLock);
                 }
                 return queue.Dequeue();
